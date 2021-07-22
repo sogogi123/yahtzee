65230ae2572372454ff1a205ef797ea3533aae8e
dunblx05 <dubblx05@naver.com>
2021-07-22 12:35:57 +0900

    스페이스바 누를때마다 주사위값 정렬되어 화면에 표시

--- yahtzee.py
+++ yahtzee.py
@@ -8,12 +8,17 @@ screen_width = 750
 screen_height = 750
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+clock = pygame.time.Clock()
+
 #주사위 변수
 dice1 = random.randint(1,6)
 dice2 = random.randint(1,6)
 dice3 = random.randint(1,6)
 dice4 = random.randint(1,6)
 dice5 = random.randint(1,6)
+
+#주사위 변수들을 list에 넣음
+dice_list = [dice1,dice2,dice3,dice4,dice5]
 
 #주사위 표시 위치 설정
 dice_y_pos = (screen_height/4)
@@ -46,74 +51,83 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
              running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                dice_list[0] = random.randint(1,6)
+                dice_list[1] = random.randint(1,6)
+                dice_list[2] = random.randint(1,6)
+                dice_list[3] = random.randint(1,6)
+                dice_list[4] = random.randint(1,6)
              
     #배경 설정
     screen.fill((255,255,255))
+    
+    dice_list.sort()
 
     #주사위 값에 따른 주사위 이미지 출력
-    if dice1 == 1:
+    if dice_list[0] == 1:
         screen.blit(d1_img,(100,dice_y_pos))
-    elif dice1 ==2:
+    elif dice_list[0] ==2:
         screen.blit(d2_img,(100,dice_y_pos))
-    elif dice1 ==3:
+    elif dice_list[0] ==3:
         screen.blit(d3_img,(100,dice_y_pos))
-    elif dice1 ==4:
+    elif dice_list[0] ==4:
         screen.blit(d4_img,(100,dice_y_pos))
-    elif dice1 ==5:
+    elif dice_list[0] ==5:
         screen.blit(d5_img,(100,dice_y_pos))
-    elif dice1 ==6:
+    elif dice_list[0] ==6:
         screen.blit(d6_img,(100,dice_y_pos))
     
-    if dice2 == 1:
+    if dice_list[1] == 1:
         screen.blit(d1_img,(200,dice_y_pos))
-    elif dice2 ==2:
+    elif dice_list[1] ==2:
         screen.blit(d2_img,(200,dice_y_pos))
-    elif dice2 ==3:
+    elif dice_list[1] ==3:
         screen.blit(d3_img,(200,dice_y_pos))
-    elif dice2 ==4:
+    elif dice_list[1] ==4:
         screen.blit(d4_img,(200,dice_y_pos))
-    elif dice2 ==5:
+    elif dice_list[1] ==5:
         screen.blit(d5_img,(200,dice_y_pos))
-    elif dice2 ==6:
+    elif dice_list[1] ==6:
         screen.blit(d6_img,(200,dice_y_pos))
 
-    if dice3 == 1:
+    if dice_list[2] == 1:
         screen.blit(d1_img,(300,dice_y_pos))
-    elif dice3 ==2:
+    elif dice_list[2] ==2:
         screen.blit(d2_img,(300,dice_y_pos))
-    elif dice3 ==3:
+    elif dice_list[2] ==3:
         screen.blit(d3_img,(300,dice_y_pos))
-    elif dice3 ==4:
+    elif dice_list[2] ==4:
         screen.blit(d4_img,(300,dice_y_pos))
-    elif dice3 ==5:
+    elif dice_list[2] ==5:
         screen.blit(d5_img,(300,dice_y_pos))
-    elif dice3 ==6:
+    elif dice_list[2] ==6:
         screen.blit(d6_img,(300,dice_y_pos))
 
-    if dice4 == 1:
+    if dice_list[3] == 1:
         screen.blit(d1_img,(400,dice_y_pos))
-    elif dice4 ==2:
+    elif dice_list[3] ==2:
         screen.blit(d2_img,(400,dice_y_pos))
-    elif dice4 ==3:
+    elif dice_list[3] ==3:
         screen.blit(d3_img,(400,dice_y_pos))
-    elif dice4 ==4:
+    elif dice_list[3] ==4:
         screen.blit(d4_img,(400,dice_y_pos))
-    elif dice4 ==5:
+    elif dice_list[3] ==5:
         screen.blit(d5_img,(400,dice_y_pos))
-    elif dice4 ==6:
+    elif dice_list[3] ==6:
         screen.blit(d6_img,(400,dice_y_pos))
 
-    if dice5 == 1:
+    if dice_list[4] == 1:
         screen.blit(d1_img,(500,dice_y_pos))
-    elif dice5 ==2:
+    elif dice_list[4] ==2:
         screen.blit(d2_img,(500,dice_y_pos))
-    elif dice5 ==3:
+    elif dice_list[4] ==3:
         screen.blit(d3_img,(500,dice_y_pos))
-    elif dice5 ==4:
+    elif dice_list[4] ==4:
         screen.blit(d4_img,(500,dice_y_pos))
-    elif dice5 ==5:
+    elif dice_list[4] ==5:
         screen.blit(d5_img,(500,dice_y_pos))
-    elif dice5 ==6:
+    elif dice_list[4] ==6:
         screen.blit(d6_img,(500,dice_y_pos))
 
     #화면 그리기
